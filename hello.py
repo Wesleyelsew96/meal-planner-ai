b744c16f741d16b18ab55bc164611be8410d1367
Wesleyelsew96 <wesleywright.lew@gmail.com>
2025-10-17 21:06:53 -0600

    chore: remove non-core files (hello.py, main.py, pyproject)

--- hello.py
+++
@@ -1,8 +0,0 @@
-def say(message: str = "Hello World") -> None:
-    """Prints the provided message to the console."""
-    print(message)
-
-
-if __name__ == "__main__":
-    # Edit the string below or the default in say() to change output.
-    say()  # Currently prints: Hello World
