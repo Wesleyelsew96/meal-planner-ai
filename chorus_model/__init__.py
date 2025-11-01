e428c8d9bde48aeff08168454c2dcc68ec0a87fd
Wesleyelsew96 <wesleywright.lew@gmail.com>
2025-11-01 00:42:20 -0600

    chore: clean workspace for meal planner

--- chorus_model/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Utilities for training and generating chorus lyrics conditioned on titles.
-
-Note: Use only data you have the legal right to use
-(public domain, your own lyrics, or properly licensed/CC works).
-"""
-
